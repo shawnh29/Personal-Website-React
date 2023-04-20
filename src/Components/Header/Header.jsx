9bfd7398377be47ca3078c1fd5c21b21d928648e
shawnh29 <Shawnhan20@gmail.com>
2023-04-19 21:10:19 -0700

    new div, button, buttons that scroll

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,15 +1,40 @@
 import React from 'react';
 import './Header.css'
 
-function Header() {
+function Header({name}) {
+
+  function scrollToAbout(e) {
+    e.preventDefault()
+    window.scrollTo({
+      top: document.querySelector('#left-div').offsetTop,
+      behavior: 'smooth'
+    })
+  }
+
+  function scrollToContact(e) {
+    e.preventDefault()
+    window.scrollTo({
+      top: document.querySelector('#right-div').offsetTop,
+      behavior: 'smooth'
+    })
+  }
+
+  function scrollToThird(e) {
+    e.preventDefault()
+    window.scrollTo({
+      top: document.querySelector('#third-div').offsetTop,
+      behavior: 'smooth'
+    })
+  }
+  
   return (
     <header className='header'>
-      <h1 className='header-title'>Shawn's Homepage</h1>
+      <h1 className='header-title'>{name}'s Homepage</h1>
       <nav className='header-nav'>
         <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Contact</a></li>
+          <li><button id='transparent-button' onClick={(e) => {scrollToThird(e)}}>Home</button></li>
+          <li><button id='transparent-button' onClick={(e) => {scrollToAbout(e)}}>About</button></li>
+          <li><button id='transparent-button' onClick={(e) => {scrollToContact(e)}}>Contact</button></li>
         </ul>
       </nav>
     </header>
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,12 +3,23 @@ import Header from '../Header/Header'
 import './Home.css'
 import pfPic from './me_justin_cat.jpg'
 
+
+
 function Home() {
+  const [buttonVal, setButtonVal] = useState(0)
+
+  function buttonClick() {
+    
+    setButtonVal(buttonVal+1)
+  }
+
+  var name = "Shawn"
+
     return (
     <div>
-      <Header />
+      <Header name={name}/>
       <div className='container'>
-        <div className='left-div'>
+        <div className='left-div' id='left-div'>
           <h2>About Me</h2>
           <p><strong>Name:</strong> Shawn Han</p>
           <p><strong>Age:</strong> 20</p>
@@ -16,12 +27,17 @@ function Home() {
           <p><strong>Year: </strong>Finishing up 3rd year</p>
           <p><strong>Interests:</strong> Consumer Electronics, Technology, Video Games, Soccer, Working out</p>
         </div>
-        <div className='right-div'>
+        <div className='right-div' id='right-div'>
           <h2>Contact Information</h2>
           <p><strong>If you would like to get in touch, the best place to reach me is my email:</strong> s1@sfu.ca</p>
           <p><strong>Phone Number:</strong> 604-123-1111</p>
           <p><a href="https://github.com/shawnh29"><strong>GitHub Link</strong></a></p>
-          <img src={pfPic} alt="Image" height="200px" width="200px"/>
+          <img src={pfPic} alt="pic" height="200px" width="200px"/>
+        </div>
+        <div className='third-div' id='third-div'>
+          <h3>Cookie Clicker Game :)</h3>
+            <p>{buttonVal}</p>
+            <button onClick={buttonClick}>Increment</button>
         </div>
       </div>
     </div>
